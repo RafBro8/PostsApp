0dd79e9b4502f16257cf9c67cc612d47d4244121
RafBro8 <rromn8@gmail.com>
2018-02-27 21:27:00 -0600

    added delete post functionality + finishes app

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost} from "../actions/index";
+import { Link } from 'react-router-dom';
+import { fetchPost, deletePost} from "../actions/index";
+
 
 class PostsShow extends Component {
     
     componentDidMount() {
         const { id } = this.props.match.params;
         this.props.fetchPost(id);
+    }
+
+    onDeleteClick() {
+        const { id } = this.props.match.params;
+
+        this.props.deletePost(id, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -18,6 +28,13 @@ class PostsShow extends Component {
 
         return (
             <div>
+                <Link to="/">Back to Index</Link>
+                <button
+                className="btn btn-danger pull-xs-right"
+                onClick={this.onDeleteClick.bind(this)}
+                >
+                    Delete Post
+                </button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -30,7 +47,7 @@ function mapStateToProps({ posts }, ownProps) {  //ownProps is the props object 
     return {post: posts[ownProps.match.params.id]};
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
 
 //componentDidMount gets executed immediately after Component is renedered
 
